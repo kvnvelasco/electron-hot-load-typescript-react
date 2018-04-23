a17feede6d4e3585ce5feca1134b0afac0d13c7e
Kevin Velasco <me@kvnvelasco.com>
2018-04-23 15:56:59 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Boilerplate for Hot Loaded React in Typescript on Electron.
+
+Built on top of electron-forge and electron-compile. 
+
+```
+npm install
+npm start
+```
+
+ 
---
+++ src/renderer/app/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import { hot } from 'react-hot-loader'
+
+export default hot(module)(() => {
+  return (
+    <div>
+      Damn!
+    </div>
+  )
+})
